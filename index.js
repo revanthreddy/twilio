cec3b3957d16eb905fa1409839fdb4ee6011b86b
revanth <revanth.reddy@gmail.com>
2015-04-04 20:22:30 -0500

    added mongo code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = require('http').createServer(app);
 server.listen(3002);
 app.use(express.bodyParser());
 //app.use(express.methodOverride());
+var MongoClient = require('mongodb').MongoClient
 var accountSid = 'AC3bf75f630d9132bcd7d03300527b0dd3';
 var authToken = 'ea04068f880d019e46fb9ce1b19dfa09';
 
@@ -16,20 +17,33 @@ app.use(express.static(__dirname + '/public/app'));
 app.use(app.router);
 
 app.get('/', function (req, res) {
-    res.send("hello");
+  res.send("hello");
 });
 
 app.post('/send', function (req, res) {
 
-  client.messages.create({
-                to: "7138262502",
-                from: "+17135974002",
-                body: "Food is served to boomer.",
-        }, function(err, message) {
-                console.log(message);
-                res.send("hello");
-  });
+  MongoClient.connect('mongodb://127.0.0.1:27017/smartbowl', function(err, db) {
+    if (err)
+    throw err;
+    var collection = db.collection('foodlog');
+    var jsonBody = req.body;
+    collection.insert({"dog" : "boomer" , "log_entry" : new Date() , "weight" : "100gm"}, {safe: true}, function(err, records) {
+      if (err) {
+        console.log(err);
+        return res.status(400).send("Failed");
+      }
+      client.messages.create({
+        to: "7138262502",
+        from: "+17135974002",
+        body: "Food is served to boomer.",
+      }, function(err, message) {
+        console.log(message);
 
+      });
+      return res.send("data logged");
+
+    });
+  });
 
 
 });
